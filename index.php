c039766ac017fe6953572e44f9450e9f29913f31
Jan-Paul Kleemans <jpkleemans@gmail.com>
2015-11-26 14:52:44 +0100

    Update code

--- index.php
+++ index.php
@@ -1,10 +1,60 @@
 <?php
 
-use Faker\Factory;
+use Doctrine\DBAL\Configuration;
+use Doctrine\DBAL\DriverManager;
+use Slim\App;
 
-require_once __DIR__ . '/vendor/autoload.php';
+require 'vendor/autoload.php';
 
-$faker = Factory::create('nl_NL');
+function debug($var)
+{
+    echo '<pre>';
+    print_r($var);
+    echo '</pre>';
+}
 
-echo $faker->name . '<br>' . $faker->address;
-// hi!
+/*
+ * DATABASE
+ */
+$config = new Configuration();
+$connectionParams = [
+    'driver' => 'pdo_mysql',
+    'host' => getenv('DB_HOST'),
+    'user' => getenv('DB_USER'),
+    'password' => getenv('DB_PASSWORD'),
+    'dbname' => getenv('DB_DATABASE'),
+];
+try {
+    $conn = DriverManager::getConnection($connectionParams, $config);
+    $db = $conn->createQueryBuilder();
+} catch (Exception $e) {
+    debug($e);
+}
+
+/*
+ * SLIM
+ */
+$app = new App;
+
+/*
+ * ROUTES
+ */
+$app->get('/connections', function ($request, $response, $args) use ($db) {
+    try {
+        $connections = $db
+            ->select('*')
+            ->from('connections')
+            ->execute()
+            ->fetchAll(PDO::FETCH_ASSOC);
+    } catch (Exception $e) {
+        debug($e);
+    }
+
+    $response = $response->withHeader('Content-type', 'application/json');
+    return $response->write(json_encode($connections));
+});
+
+/*
+ * RUN
+ */
+$app->run();
